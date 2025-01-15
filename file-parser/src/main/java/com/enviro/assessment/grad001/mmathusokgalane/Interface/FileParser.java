f6c83989ba1f82745578ac83cf6a50ddfc6f0110
Mmathuso Angelina <48378806+Meythusoo98@users.noreply.github.com>
2025-01-15 13:05:17 +0200

    Delete file-parser/src/main directory

--- file-parser/src/main/java/com/enviro/assessment/grad001/mmathusokgalane/Interface/FileParser.java
+++
@@ -1,9 +0,0 @@
-package com.enviro.assessment.grad001.mmathusokgalane.Interface;
-import java.io.File;
-import java.net.URI;
-
-public interface FileParser {
-    void parseCSV(File csvFile);
-    File convertCSVDataToImage(String base64ImageData);
-    URI createImageLink(File fileImage);
-}
